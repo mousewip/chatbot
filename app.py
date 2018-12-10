178f3d0df145b7695b2fafcee28356ee2caa1c9a
MouseWip <mousewip@gmail.com>
2018-12-10 23:33:35 +0700

    port

--- app.py
+++ app.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 import os
 from Project import *
+from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10095))
-    app.run('127.0.0.1', port=port)
+    # app.run('127.0.0.1', port=port, threaded=True)
     # app.run(ssl_context='adhoc', port=port) # HTTPS
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('127.0.0.1', 10095), app)
+    http_server.serve_forever()
