ce12a75ad93bd26a14deea69b23b32571a465f48
MouseWip <mousewip@gmail.com>
2018-12-10 22:28:42 +0700

    update port

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import os
 from Project import *
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8001))
-    app.run('192.168.1.232', port=port)
+    port = int(os.environ.get("PORT", 10095))
+    app.run('127.0.0.1', port=port)
     # app.run(ssl_context='adhoc', port=port) # HTTPS
