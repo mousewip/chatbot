b1ae3ce4eca64facf7199591d375955da29aa706
MouseWip <mousewip@gmail.com>
2018-12-11 11:12:53 +0700

    model conv

--- Project/__init__.py
+++ Project/__init__.py
@@ -2,7 +2,7 @@
 __version__ = '1.0'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, random
 from src.text_classification_predict import TextClassificationPredict
 
 app = Flask('Project', template_folder='Views')
@@ -34,9 +34,25 @@ def Predict(pred_src):
     global tcp
     rs, accu = tcp.predict_conv(pred_src)
 
-    if rs == "tro_giup" and accu <= 0.5:
+    if rs == "mo_ta_trieu_chung":
         rs = tcp.predict(pred_src)
-    print(rs)
+        return rs
+    else:
+        # chao_hoi, tro_giup, ket_thuc
+        if rs == "chao_hoi":
+            lst_rs = []
+            lst_rs.append("Xin chào bạn, bạn cần trợ giúp gì?")
+            lst_rs.append("Xin chào bạn, bạn cần hỗ trợ gì?")
+            lst_rs.append("Bot có thể giúp gì cho bạn?")
+            lst_rs.append("Supper Bot Y tế có mặt. Bạn cần gì nè?")
+            rs = random.choice(lst_rs)
+
+        elif rs == "ket_thuc":
+            rs = "Cám ơn bạn đã sử dụng dịch vụ."
+        elif rs == "tro_giup" and accu >= 0.5:
+            rs = "Bạn cần hỗ trợ gì?"
+        else:
+            rs = "Xin lỗi, bạn vui lòng hỏi câu khác nhé!"
     return rs
 
 
